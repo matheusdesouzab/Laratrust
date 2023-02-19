790806cd32c05fc5ecd1ae32bed1d8a5876d4583
Matheus de Souza <matheusdesouza187@gmail.com>
2023-02-19 16:32:29 -0300

    feat: implementação de novos elementos na interface

--- resources/views/permissions-roles/create.blade.php
+++ resources/views/permissions-roles/create.blade.php
@@ -1,14 +1,24 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="col-lg-10 mx-auto mt-5">
+    <div class="col-lg-11 mx-auto mt-5">
         <div class="card p-4">
             <div class="d-flex justify-content-between align-items-center">
-                <h3 class="">Vincular permissão a função</h3>
-                <a href="{{ route('permissions.index') }}" class="btn btn-success me-2">Ver premissões</a>
+                <h4 class="fw-bold mb-0">Vincular permissão a função</h4>
+                <a href="{{ route('permissions.index') }}" class="btn btn-primary me-2 fw-bold">Ver permissões</a>
             </div>
             <hr>
-            <form class="row g-3 p-4" method="POST" action="{{ route('permissions-roles.store') }}">
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Ops!</strong> Você precisa corrigir os seguintes erros<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form class="row g-3 px-2" method="POST" action="{{ route('permissions-roles.store') }}">
                 @csrf
                 <div class="col-md-6">
                     <label for="role" class="form-label fw-bold">Função a ser atribuida uma permissão</label>
@@ -29,7 +39,7 @@
                     </select>
                 </div>
                 <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Vincular permissão a função</button>
+                    <button type="submit" class="btn btn-success">Vincular permissão a função</button>
                 </div>
             </form>
         </div>
