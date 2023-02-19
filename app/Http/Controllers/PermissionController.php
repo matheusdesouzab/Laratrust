9fe2d84440c7b5721fb3078842806c4bfa5e611a
Matheus de Souza <matheusdesouza187@gmail.com>
2023-02-19 15:58:39 -0300

    feat: inclusão do PermissionStoreRequest no store

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PermissionStoreRequest;
 use App\Models\Permission;
 use Illuminate\Http\Request;
 
@@ -34,7 +35,7 @@ class PermissionController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PermissionStoreRequest $request)
     {
         Permission::create([
             'name' => $request->name,
