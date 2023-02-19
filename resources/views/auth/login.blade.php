d56d7427ec42835a13bde9c8c54d9526b5e39c33
Matheus de Souza <matheusdesouza187@gmail.com>
2023-02-19 09:12:38 -0300

    fix: correções na interface do login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,19 +4,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body class="bg-light">
-    <div class="vh-100 d-flex justify-content-center align-items-center bg-dark">
+    <div class="vh-100 d-flex justify-content-center align-items-center bg-secondary">
         <div class="container">
             <div class="row">
-                <div class="col-sm-9 col-md-7 col-lg-8 mx-auto">
-                    <div class="card border-0 shadow rounded-3 my-5">
+                <div class="col-sm-9 col-md-6 mx-auto">
+                    <div class="card border-0 shadow-sm rounded-3 my-5">
                         <div class="card-body p-4 p-sm-5">
-                            <h5 class="card-title text-center mb-5 fw-bold fs-5">Acessar conta</h5>
+                            <h5 class="card-title fw-bold fs-5">Acessar conta</h5>
+                            <hr>
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
                                 <div class="form-floating mb-3">
@@ -31,7 +32,8 @@
                                 </div>
 
                                 <div class="d-grid">
-                                    <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Login</button>
+                                    <button class="btn btn-primary btn-login text-uppercase fw-bold"
+                                        type="submit">Login</button>
                                 </div>
                             </form>
                         </div>
