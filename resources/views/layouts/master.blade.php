ccdf81ca400c1f62c9b9ef01d285c4532d90b6b7
Matheus de Souza <matheusdesouza187@gmail.com>
2023-02-19 10:06:06 -0300

    feat: implementação da nova interface no layout master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,42 +7,57 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
 
 <body class="bg-light">
     <main class="container-fluid p-0">
-        <nav class="navbar navbar-expand-lg bg-primary">
-            <div class="container-fluid">
-                <a class="navbar-brand fw-bold text-white" href="#">Laratrust</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link fw-bold text-white" href="{{ route('roles.index') }}">Funções</a>
+        <div class="row p-0">
+            <div class="col-lg-3 vh-100 bg-dark">
+                <div class="flex-shrink-0 p-3">
+                    <h4 class="fw-bold text-center text-warning mb-4">Permissões</h4>
+                    <ul class="list-unstyled p-3 bg-light rounded-3">
+                        <li class="mb-2">
+                            <button class="btn btn-toggle rounded-2 collapsed w-100 text-start fw-semibold"
+                                data-bs-toggle="collapse" data-bs-target="#funcoes-collapse" aria-expanded="true">
+                                <i class="bi bi-caret-right"></i> Funções
+                            </button>
+                            <div class="collapse mb-3 p-3 mt-2 rounded-2 bg-white border" id="funcoes-collapse">
+                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <li><a href="{{ route('roles.create') }}"
+                                            class="link-dark text-decoration-none rounded fw-semibold d-block p-2 mb-2 bg-white"><i
+                                                class="bi bi-plus"></i> Criar função</a></li>
+                                    <li><a href="{{ route('roles.index') }}"
+                                            class="link-dark text-decoration-none rounded fw-semibold d-block p-2 bg-white"><i
+                                                class="bi bi-list"></i> Listagem</a></li>
+                                </ul>
+                            </div>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link fw-bold text-white" href="{{ route('permissions.index') }}">Permissões</a>
+                        <li class="mb-2">
+                            <button class="btn btn-toggle rounded-2 collapsed w-100 text-start fw-semibold"
+                                data-bs-toggle="collapse" data-bs-target="#permissao-collapse" aria-expanded="true">
+                                <i class="bi bi-caret-right"></i> Permissões
+                            </button>
+                            <div class="collapse mb-3 p-3 mt-2 rounded-2 bg-white border" id="permissao-collapse">
+                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <li><a href="{{ route('permissions.create') }}"
+                                            class="link-dark text-decoration-none rounded fw-semibold d-block p-2 mb-2 bg-white"><i
+                                                class="bi bi-plus"></i> Criar permissão</a></li>
+                                    <li><a href="{{ route('permissions.index') }}"
+                                            class="link-dark text-decoration-none rounded fw-semibold d-block p-2 bg-white"><i
+                                                class="bi bi-list"></i> Listagem</a></li>
+                                </ul>
+                            </div>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link fw-bold text-white" href="{{ route('permissions-roles.create') }}">Vincular</a>
-                        </li>                       
-                        <li class="nav-item dropdown me-4">
-                            <a class="nav-link fw-bold text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                              {{ Auth()->user()->name }}
-                            </a>
-                            <ul class="dropdown-menu">
-                              <li><a class="dropdown-item" href="{{ route('users.index') }}">Listagem</a></li>
-                              <li><a class="dropdown-item" href="{{ route('users.create') }}">Criar</a></li>
-                            </ul>
-                          </li>                  
                     </ul>
                 </div>
             </div>
-        </nav>
-        @yield('content')
+            <div class="col-lg-9 bg-light">
+                <div class="row">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
